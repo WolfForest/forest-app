15a1cd1456393c8a02bf2ec82991accb270d8a98
forest <mr.wolfforest@gmail.com>
2017-11-27 13:28:25 +0500

    actions

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var actions = [
+    'scratches the butt',
+    'howls at the moon',
+    'snorts',
+    'wags his tail',
+    'growl at the friend',
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({
       status: 'ok',
       time: Date.now(),
       list: [
-          {uid: 1003, name: 'Wolf Forest'},
-          {uid: 1004, name: 'Simon Fawkes'},
-          {uid: 1005, name: 'Jarrod Samms'},
+          {uid: 1003, name: 'Wolf Forest', action: actions[Math.floor(Math.random()*actions.length)]},
+          {uid: 1004, name: 'Simon Fawkes', action: actions[Math.floor(Math.random()*actions.length)]},
+          {uid: 1005, name: 'Jarrod Samms', action: actions[Math.floor(Math.random()*actions.length)]},
       ]
   });
 });
