8948f67f222902ea902c679b3429b00add106d0e
forest <mr.wolfforest@gmail.com>
2017-11-27 15:23:07 +0500

    profile

--- routes/users.js
+++ routes/users.js
@@ -26,7 +26,11 @@ router.get('/', function(req, res, next) {
           {uid: 1005, name: 'Jarrod Samms', action: actions[Math.floor(Math.random()*actions.length)]},
       ],
       messages: [
-          {uid: 1003, from: 'Wolf Forest', title: 'Title', body: 'Some body message'},
+          {uid: 1003, from: 'Wolf Forest', title: 'Title 1', body: 'Some body message', image: 'http://lorempixel.com/470/250/nature/1/'},
+          {uid: 1003, from: 'Wolf Forest', title: 'Title 2', body: 'Some body message', image: 'http://lorempixel.com/470/250/nature/2/'},
+          {uid: 1003, from: 'Wolf Forest', title: 'Title 3', body: 'Some body message', image: 'http://lorempixel.com/470/250/nature/3/'},
+          {uid: 1003, from: 'Wolf Forest', title: 'Title 4', body: 'Some body message', image: 'http://lorempixel.com/470/250/nature/4/'},
+          {uid: 1003, from: 'Wolf Forest', title: 'Title 5', body: 'Some body message', image: 'http://lorempixel.com/470/250/nature/5/'},
       ],
       profile: {
           ava: 'https://forest-app.herokuapp.com/images/ys5wPIBu_400x400.jpg',
