5ed24d9c083fe1cf0c6c209c9f1e022e8687c5a9
forest <mr.wolfforest@gmail.com>
2017-11-27 15:00:24 +0500

    profile

--- routes/users.js
+++ routes/users.js
@@ -18,6 +18,12 @@ router.get('/', function(req, res, next) {
           {uid: 1003, name: 'Wolf Forest', action: actions[Math.floor(Math.random()*actions.length)]},
           {uid: 1004, name: 'Simon Fawkes', action: actions[Math.floor(Math.random()*actions.length)]},
           {uid: 1005, name: 'Jarrod Samms', action: actions[Math.floor(Math.random()*actions.length)]},
+          {uid: 1003, name: 'Wolf Forest', action: actions[Math.floor(Math.random()*actions.length)]},
+          {uid: 1004, name: 'Simon Fawkes', action: actions[Math.floor(Math.random()*actions.length)]},
+          {uid: 1005, name: 'Jarrod Samms', action: actions[Math.floor(Math.random()*actions.length)]},
+          {uid: 1003, name: 'Wolf Forest', action: actions[Math.floor(Math.random()*actions.length)]},
+          {uid: 1004, name: 'Simon Fawkes', action: actions[Math.floor(Math.random()*actions.length)]},
+          {uid: 1005, name: 'Jarrod Samms', action: actions[Math.floor(Math.random()*actions.length)]},
       ],
       profile: {
           ava: 'https://forest-app.herokuapp.com/images/ys5wPIBu_400x400.jpg',
